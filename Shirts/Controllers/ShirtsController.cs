3c500cb29f1922187b797b14e6d264ea84c6817b
WinstonMuijs <56136891+WinstonMuijs@users.noreply.github.com>
2023-09-07 12:11:30 +0200

    Web Api with Controllers: Routing
    Starting a Web api with Controllers - MVC style - added the Routing/Mapping of the Http-requests tot the action Methods in the Controllers Folder.

---
+++ Shirts/Controllers/ShirtsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shirts.Controllers
+{
+   
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController : ControllerBase
+    {
+        
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Get All the shirts from the e-shop";
+        }
+        
+        [HttpGet("{id}")]
+        public string GetShirt(int id)
+        {
+            return $"Get a shirt from the e-shop with id: {id}";
+        }
+        
+        [HttpPost]
+        public string PostShirts()
+        {
+            return "Post a shirt from the e-shop";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirts(int id)
+        {
+            return $"Update a shirt from the e-shop with id: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirts(int id)
+        {
+            return $"Delete ashirts from the e-shop with id: {id}";
+        }
+    }
+}
---
+++ Shirts/Program.cs
@@ -0,0 +1,21 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+/*Als er iets aan dependencies nodig is voor de Controllers wordt dat geleverd
+ * vanuit de container door AddControllers in .NET
+ * */
+
+
+app.Run();
+
+
