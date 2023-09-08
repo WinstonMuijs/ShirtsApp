b67092f910753eac1f3c6c80a7791028e7aea160
WinstonMuijs <56136891+WinstonMuijs@users.noreply.github.com>
2023-09-08 14:13:22 +0200

    Added Model Validation

--- Shirts/Models/Shirt.cs
+++ Shirts/Models/Shirt.cs
@@ -1,15 +1,19 @@
-﻿namespace Shirts.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Shirts.Models.Validations;
+
+namespace Shirts.Models
 {
 	public class Shirt
 	{
 		public int ShirtId { get; set; }
 
+        [Required]
         public string? Brand { get; set; }
-
+        [Required]
         public string? Color { get; set; }
-
+        [Required]
         public string? Gender { get; set; }
-
+        [Shirt_CorrectSizing]
         public int Size { get; set; }
 
         public double Price { get; set; }
